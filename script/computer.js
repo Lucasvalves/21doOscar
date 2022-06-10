7e12fccf3740ef4dfafacfb4be23f2c4acde507e
Lucas Alves <lucasvelosoalves@outlook.com>
2022-06-10 16:37:10 -0300

    Estrutura Final

---
+++ script/computer.js
@@ -0,0 +1,249 @@
+// Arquivo script no modo player vs computer
+const flipSound = document.getElementById('flipSound');
+var single =  document.getElementById("single");
+var nameTurnS =  document.getElementById("nameTurn");
+var gameS =  document.getElementById("game"); 
+var openMenuS =  document.getElementById("openMenu");
+var rulesMenuS =  document.getElementById("rulesMenu");
+var newGameMenuS =  document.getElementById("newGameMenu");
+var quitS =  document.getElementById("quitS");
+var scrRegras =  document.getElementById("scrRegras");
+var scrStart =  document.getElementById("scrStart");
+var scrRanking = document.getElementById('_scrRanking');
+var screenGame= document.getElementById('__container');
+var buttonS1 = document.getElementById('buttonS1'),
+    buttonS2 = document.getElementById('buttonS2'),
+    buttonS3 = document.getElementById('buttonS3');
+var cont = 0;
+
+function flipGameSound() {
+    flipSound.duration = 0.9
+    flipSound.volume = 0.1
+    flipSound.play()
+}
+single.onclick = () => {
+    flipGameSound(); 
+    var persName = document.getElementById('persName');
+    var scrloading = document.getElementById('scrloading');  
+
+    scrStart.style.display = 'none';
+    scrloading.style.display = 'block ';
+    setTimeout(() => {
+        scrloading.style.display = 'none ';
+    }, 2800)
+    setTimeout(() => {
+    persName.style.display = 'block';
+    }, 2800)
+}
+var playerTurn1 = document.getElementById('playerTurn1');
+var input1 = document.querySelector('#inserName1')
+var scrRegras =  document.getElementById("scrRegras");
+nameTurnS.onclick = () => {
+    flipGameSound();
+    axios.get(`https://api.github.com/users/${input1.value}`)
+    .then(response => {
+        document.querySelector('#inserName1').innerHTML = ` ${response.data.login}`
+         if(response.data.name === null) {
+            document.querySelector('#inserName1').innerHTML = `Nome não cadastrado`
+         }
+         else{
+            document.querySelector('#inserName1').innerHTML = ` ${response.data.login}`
+            scrRegras.style.display = 'block'; 
+            persName.style.display = 'none';
+            playerTurn1.innerHTML = `Your time ${input1.value} !`;
+         }
+    })
+    .catch(() => {
+        alert(`GitHub não encontrado`)
+    })
+}
+buttonS1.value = 1;
+buttonS2.value = 2;
+buttonS3.value = 3;
+
+function buttonS2click(parametro) {
+    buttonS1.value = +parametro + 1;
+    buttonS2.value = +parametro + 2;
+    buttonS3.value = +parametro + 3;
+}
+onload = function (){
+    var nomePers = localStorage.getItem("nome");
+    var pnome1 = document.getElementById('pnome1');
+    pnome1.innerHTML = nomePers;
+
+    var pontParse = localStorage.getItem("pontV");         
+    var scoreV = document.getElementById('scoreV');
+    scoreV.innerHTML = pontParse;
+
+    var pontParseM = localStorage.getItem("pontVM");         
+    var scoreVM = document.getElementById('scoreVM');
+    scoreVM.innerHTML = pontParseM;
+}
+var victory1 = false;
+buttonS1.onclick = () => {
+    flipGameSound();
+    cont = buttonS1.value;
+    if(buttonS1.value == cont && cont == 21){     
+   
+        nameV();    
+        pontuacaov();
+
+    }      
+    if(buttonS1.value > 21 && cont > 21){
+        var defeats = document.getElementById('defeats');
+        screenGame.style.display = 'none';
+        defeats.style.display = 'block'; 
+
+        localStorage.setItem("nome", "computer");
+         var nome = localStorage.getItem("nome");         
+         var pnome1 = document.getElementById('pnome1');
+         pnome1.innerHTML = nome;      
+    }
+    buttonS2click(buttonS1.value);
+    computerchoice(buttonS1.value);
+}
+buttonS2.onclick = () => {
+    flipGameSound();
+    cont = buttonS2.value;
+    if(buttonS2.value == cont && cont == 21){          
+        victory.style.display = 'block';
+        screenGame.style.display = 'none';
+        setTimeout(() => {
+                 victory.style.display = 'none';
+                 scrRanking.style.display = 'block';               
+         }, 2800)
+       nameV();  
+       pontuacaov();
+    }
+    if(buttonS2.value > 21 && cont > 21){
+        screenGame.style.display = 'none';
+        defeats.style.display = 'block';
+        localStorage.setItem("nome", "computer");
+         var nome = localStorage.getItem("nome");      
+         var pnome1 = document.getElementById('pnome1');
+         pnome1.innerHTML = nome;
+    }
+    buttonS2click(buttonS2.value)
+    computerchoice(buttonS2.value);
+} 
+buttonS3.onclick = () => {
+    flipGameSound();
+    cont = buttonS3.value;
+    if(buttonS3.value == cont && cont == 21){
+         victory.style.display = 'block';
+        screenGame.style.display = 'none'; 
+        
+        setTimeout(() => {
+                 victory.style.display = 'none';
+                 scrRanking.style.display = 'block';
+                                
+         }, 2800)
+        nameV();
+        pontuacaov();
+
+    }
+    if(buttonS3.value > 21 && cont > 21){
+        screenGame.style.display = 'none';
+        defeats.style.display = 'block';
+
+        localStorage.setItem("nome", "computer");
+         var nome = localStorage.getItem("nome");        
+         var pnome1 = document.getElementById('pnome1');
+         pnome1.innerHTML = nome;  
+    }
+    buttonS2click(buttonS3.value)
+    computerchoice(buttonS3.value);
+}
+function pontuacaov(){
+    
+    localStorage.setItem("pontV", vic);
+    var pontV = localStorage.getItem("pontV");         
+    var scoreV = document.getElementById('scoreV');
+     victory1 = true;
+    if(victory1 == true){
+        var x = 0;
+        pontV = parseInt(pontV) + x;  
+    }
+    scoreV.innerHTML = pontV;  
+}
+function nameV(){
+    localStorage.setItem("nome", input1.value);
+    var nome = localStorage.getItem("nome");         
+    var pnome1 = document.getElementById('pnome1');
+    pnome1.innerHTML = nome;
+}
+function computerchoice(cont){
+    var numberPC = document.getElementById('numberPC');
+    playerTurn1.innerHTML = null;
+    playerTurn1.innerHTML = "Computer is playing";
+    var x = new Array(+cont + 1, +cont + 2, +cont + 3),
+        y = x[Math.floor(Math.random()*3)]
+
+    setTimeout(() => {
+        buttonS1.value = y + 1;
+        buttonS2.value = y + 2;
+        buttonS3.value = y + 3;  
+        playerTurn1.innerHTML = `Your time ${input1.value} !`;  
+    },3000)
+    setTimeout(() => {       
+        numberPC.innerHTML = `${y} `;  
+    },2500)   
+}
+gameS.onclick = () => { 
+    flipGameSound();
+    scrRegras.style.display = 'none';
+    menuAtalho.style.display = 'block'; 
+    screenGame.style.display = 'block';  
+}
+menuAtalho.onclick = () => {
+    flipGameSound();
+    screenGame.style.display = 'none';
+    menuOpt.style.display = 'block'; 
+    menuAtalho.style.display = 'none';  
+    defeats.style.display = 'none';
+    scrRanking.style.display = 'none'; 
+    victory.style.display = 'none';
+}
+openMenuS.onclick = () => {
+    flipGameSound();
+    screenGame.style.display = 'none';
+    menuOpt.style.display = 'block'; 
+    menuAtalho.style.display = 'none';  
+    defeats.style.display = 'none';
+    scrRanking.style.display = 'none'; 
+}
+newGameMenuS.onclick = () => {
+    flipGameSound();
+    var menuOpt =  document.getElementById("menuOpt");
+    screenGame.style.display = 'block';
+    menuOpt.style.display = 'none';  
+    menuAtalho.style.display = 'block'; 
+    buttonS1.value = 1;
+    buttonS2.value = 2; 
+    buttonS3.value = 3;
+} 
+rulesMenuS.onclick = () => {
+    flipGameSound();
+    menuOpt.style.display = 'none';
+    scrRegras.style.display = 'block';
+}
+//function para retornar ao menu start com os modos de jogo
+returStartS.onclick= () => {
+    menuAtalho.style.display = 'none';
+    menuOpt.style.display = 'none';
+    scrStart.style.display = 'block'; 
+    scrStart.style.display = 'flex'; 
+}
+//function para sair do jogo singleplayer
+quitS.onclick = () => {
+    flipGameSound();
+    var quitGameS =  document.getElementById("quitGameS");
+    menuAtalho.style.display = 'none';
+    menuOpt.style.display = 'none';  
+    quitGameS.innerHTML = "bye see you next time!";
+    quitGameS.style.fontSize = "50px";
+}
+
+
+
+
---
+++ script/players.js
@@ -0,0 +1,247 @@
+// Arquivo script no modo player vs player
+const flipSoundM = document.getElementById('flipSoundM');
+var defeat = document.getElementById('defeatM');
+var mnOpt =  document.getElementById("menuOptM");
+var mnAtalho =  document.getElementById("menuAtalhoM");
+var scr =  document.getElementById("scrStart");
+var scrRankin = document.getElementById('_scrRanking');
+var srcRulesM =  document.getElementById("scrRegrasM");
+var scrGameM = document.getElementById('__containerM');
+var quitM =  document.getElementById("quitM");
+var buttonM1 = document.getElementById('buttonM1'),
+    buttonM2 = document.getElementById('buttonM2'),
+    buttonM3 = document.getElementById('buttonM3');
+var num = 0;
+
+//função de som de click
+function flipGameSoundM() {
+
+    flipSoundM.duration = 0.9
+    flipSoundM.volume = 0.1
+    flipSoundM.play()
+}
+////function do inputs para o multiplayer
+multi.onclick = () => {
+
+   flipGameSoundM();
+   var persNameM = document.getElementById('persNameM');  
+   var inforName = document.getElementById('infor-nameM'); 
+   var loadM = document.getElementById('loadM'); 
+
+   scr.style.display = 'none ';
+   loadM.style.display = 'block ';
+   setTimeout(() => {
+      loadM.style.display = 'none ';
+   }, 2800)
+   setTimeout(() => {
+       persNameM.style.display = 'block';
+       inforName.innerHTML = "inform the names:";
+       }, 2800) 
+}
+var inputM1 = document.getElementById('inserName1M');
+var input2M = document.getElementById('inserName2M');
+var playerT1M = document.getElementById('playerTurn1M');
+//function para verificar os nomes dos usuarios e se eles possuem git hub
+nameTurnM.onclick = () => {
+   flipGameSoundM();
+   setTimeout(() => {   
+     playerT1M.innerHTML = `Click on the chosen number one at <br> a time!`;  
+   },4000)  
+   axios.get(`https://api.github.com/users/${inputM1.value}`)
+   .then(response => {
+         document.querySelector('#inserName1M').innerHTML = `${response.data.login}`
+         document.querySelector('#inserName2M').innerHTML = `${response.data.login}`
+         if(response.data.login === null) {
+            document.querySelector('#inserName1M').innerHTML = `GitHub player1 não cadastrado`            
+         }
+         else{
+           if(input2M.value !== ""){
+            document.querySelector('#inserName2M').innerHTML = ` ${response.data.login}`
+            document.querySelector('#inserName1M').innerHTML = `${response.data.login}`        
+            persNameM.style.display = 'none';
+            srcRulesM.style.display = 'block';
+            playerT1M.innerHTML = `Hello ${inputM1.value} and ${input2M.value}  !`;  
+           } 
+           else{
+            alert(`GitHub do player 2 não encontrado `)  
+           }
+           
+         } 
+   })
+   .catch(() => {
+         alert(`GitHub não encontrado`)
+   })         
+}
+//function para depois de clicar no botão de regras o jogo iniciar
+gameM.onclick = () => {
+   flipGameSoundM();
+   srcRulesM.style.display = 'none'; 
+   mnAtalho.style.display = 'block'; 
+   scrGameM.style.display = 'block';  
+}
+//definindo valores inicias para os butões
+buttonM1.value = 1;
+buttonM2.value = 2;
+buttonM3.value = 3;
+
+function buttonM2click(model) {
+   buttonM1.value = +model + 1;
+   buttonM2.value = +model + 2;
+   buttonM3.value = +model + 3;
+}
+//function de click do primeiro botão do multiplayer
+var victory1 = false;
+buttonM1.onclick = () => {
+   flipGameSoundM();
+   playerTurn()
+   num = buttonM1.value;  
+   if(buttonM1.value == num && num == 21){    
+
+       var vitory = document.getElementById('victory');
+       vitory.style.display = 'block';
+       scrGameM.style.display = 'none';
+       setTimeout(() => {
+           var scrRankin = document.getElementById('_scrRanking'); 
+                vitory.style.display = 'none';
+                scrRankin.style.display = 'block';                    
+        }, 2800) 
+        nameM();
+        pontuacaoV();
+   }        
+   if(buttonM1.value > 21 && num > 21){
+       scrGameM.style.display = 'none';
+       defeat.style.display = 'block'; 
+   }
+   buttonM2click(buttonM1.value);
+}
+//function de click do segundo botão do multiplayer
+buttonM2.onclick = () => {
+   flipGameSoundM();
+   playerTurn()
+   num = buttonM2.value;
+   if(buttonM2.value == num && num == 21){   
+    var vitory = document.getElementById('victory');
+    vitory.style.display = 'block';
+    scrGameM.style.display = 'none';
+       setTimeout(() => {
+           var scrRankin = document.getElementById('_scrRanking');  
+                vitory.style.display = 'none';
+                scrRankin.style.display = 'block';                               
+        }, 2800)
+        nameM();
+        pontuacaoV();
+   }
+   if(buttonM2.value > 21 && num > 21){
+       scrGameM.style.display = 'none';
+       defeat.style.display = 'block';
+   }
+   buttonM2click(buttonM2.value);
+}
+//function de click do terceiro botão do multiplayer
+buttonM3.onclick = () => {
+   flipGameSoundM();
+   playerTurn()
+   num = buttonM3.value;    
+   if(buttonM3.value == num && num == 21){
+       var vitory = document.getElementById('victory');
+       vitory.style.display = 'block';
+       scrGameM.style.display = 'none';
+       setTimeout(() => {
+           var scrRankin = document.getElementById('_scrRanking');  
+                vitory.style.display = 'none';
+                scrRankin.style.display = 'block';          
+        }, 2800) 
+        nameM();
+        pontuacaoV(); 
+   }
+   if(buttonM3.value > 21 && num > 21){
+       scrGameM.style.display = 'none';
+       defeat.style.display = 'block';
+   }
+   buttonM2click(buttonM3.value)
+}
+//function de click de triunfo do mplayer ou computer
+function pontuacaoV(){
+   var vict = 1;
+   localStorage.setItem("pontVM", vict);
+   var pontVM = localStorage.getItem("pontVM");         
+   var scoreVM = document.getElementById('scoreVM');
+    victory1 = true;
+   if(victory1 == true){
+       var i = 0;
+       pontVM = parseInt(pontVM) + i;  
+   }
+   scoreVM.innerHTML = pontVM;  
+} 
+//function que leva o nome do jogador vencedor seja p computer ou player para o localStorage
+function nameM(){
+   localStorage.setItem("mNome", inputM1.value);
+   var nomeM = localStorage.getItem("mNome");         
+   var pnome2= document.getElementById('pnome2');
+   pnome2.innerHTML = nomeM;
+}
+function playerturn(){ 
+   var vezjogador = 0;
+       vezjogador = buttonM1.value % 2;
+   if(vezjogador == 0 ){  
+      playerT1M.innerHTML = `Vez ${inputM1.value} !`;  
+   }
+   if(vezjogador == 1 ){  
+      playerT1M.innerHTML = `Vez ${input2M.value} !`;  
+   }
+}
+//function do campo de atalho para o menu
+mnAtalho.onclick = () => {
+   flipGameSoundM();
+   scrGameM.style.display = 'none';
+   mnOpt.style.display = 'block'; 
+   mnAtalho.style.display = 'none';
+   defeat.style.display = 'none';
+   scrRankin.style.display = 'none';  
+   victory.style.display = 'none'; 
+}
+//function que abre o menu
+openMenuM.onclick = () => {
+   flipGameSoundM();
+    scrGameM.style.display = 'none';
+    mnOpt.style.display = 'block'; 
+    mnAtalho.style.display = 'none';
+    defeat.style.display = 'none';
+    scrRankin.style.display = 'none';   
+ }
+//function que inicia novo jogo
+newGameMenuM.onclick = () => {
+   flipGameSound();
+   scrGameM.style.display = 'block';
+   mnOpt.style.display = 'none';  
+   mnAtalho.style.display = 'block'; 
+   scrRanking.style.display = 'none';  
+   buttonM1.value = 1;
+   buttonM2.value = 2;
+   buttonM3.value = 3;
+}
+//function que mostra as regras do menu
+rulesMenuM.onclick = () => {
+   flipGameSoundM();
+   mnOpt.style.display = 'none';
+   srcRulesM.style.display = 'block';
+}
+
+//function para retornar ao menu start com os modos de jogo
+returStartM.onclick= () => {
+   mnAtalho.style.display = 'none';
+   mnOpt.style.display = 'none';
+   scrStart.style.display = 'block'; 
+   scrStart.style.display = 'flex'; 
+}
+//function para sair do jogo multiplayer 
+quitM.onclick = () => {
+   flipGameSoundM();
+   var qtGameM =  document.getElementById("quitGameM");
+   mnAtalho.style.display = 'none';
+   mnOpt.style.display = 'none';  
+   qtGameM.style.display = 'block';
+   qtGameM.innerHTML = "bye see you next time!";
+   qtGameM.style.fontSize = "50px";
+   
+}
